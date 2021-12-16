e17c5ae3d71bafacc36bcdb9858416c63df4cfd9
Paul <89970110+asemaninarenji2@users.noreply.github.com>
2021-12-16 15:39:28 +1100

    this svg without width and height doesn't work

--- extra-files/CartIcon.js
+++ extra-files/CartIcon.js
@@ -1,6 +1,9 @@
 const CartIcon = () => {
   return (
     <svg
+    //missing width and height
+    width="24"
+        height="24"
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 20 20'
       fill='currentColor'
